dc492d94742bdfc0f761e4671aa56fc685f3e4c1
umapan <umapanpoosuwan@gmail.com>
2017-04-18 10:11:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const apiai = require('apiai');
-const xml2js = require('xml2js');
+const xml2js = require('xml2js'); 
+const natural = require('natural'),tokenizer = new natural.WordPunctTokenizer();
 
 const app = express();
 app.use(bodyParser.json());
@@ -62,7 +63,8 @@ function sendMessage(event) {
       method: 'POST',
       json: {
         recipient: {id: sender},
-        message: {text: aiText}
+        message: z
+
       }
     }, (error, response) => {
       if (error) {
@@ -82,73 +84,42 @@ function sendMessage(event) {
 
 /* Webhook for API.ai to get response from the 3rd party API */
 app.post('/ai', (req, res) => {
-  console.log('*** Webhook for api.ai query ***');
-  console.log(req.body.result);
-  var dwname = '';  var stock_name = '';  var msg = '';
+  var dwname = '';  
+  var stock_name = '';  
+  var msg = '';
   if (req.body.result.action == 'AskStock') {
     console.log('*** Stock Symbols ***');
     
-    stock_name = req.body.result.parameters['stockname'];
+    var stock_name = req.body.result.parameters['stockname'];
+    var closing_price = req.body.result.parameters['ClosedPrice'];
+    var last_price = req.body.result.parameters['LastPrice'];
 
     var callStockGOGL = [];
-    //var restUrl = 'https://google-stocks.herokuapp.com/?code=BKK:'+stock_name+'&format=json';
-    //var restUrl = 'https://stocksymbols.herokuapp.com/?symbol=BKK:'+stock_name+'&format=json';
     var restUrl = 'http://www.google.com/finance/info?nfotype=infoquoteall&q=INDEXBKK:'+stock_name+'&callback=?';
 
     request({url: restUrl,json: true }, function (error, response, body) {
-      if (!error && response.statusCode == 200 && body) { 
+      if (!error && response.statusCode == 200 && body[0]) { 
         callStockGOGL.push(body.substring(3));
         var result = JSON.parse(callStockGOGL);
-        
-        msg = 'ชื่อหุ้น ' + result[0].t + ' ราคา ' + result[0].l + ' บาท เปลี่ยนแปลง ' + result[0].c + ' บาท ('+ result[0].cp+'%) ข้อมูล ณ ' + result[0].lt;
-        console.log(msg);
-        return res.json({speech: msg,displayText: msg,source: 'stock_name'});
-        
-        /*
-        // thai stock price + BKK
-        if (body[0].e === 'BKK') {
-          var msg = 'ชื่อหุ้น ' + body[0].t + ' ราคา ' + body[0].l + ' บาท เปลี่ยนแปลง ' + body[0].c + ' บาท ('+ body[0].cp+'%) ข้อมูล ณ ' + body[0].lt;
-          return res.json({speech: msg,displayText: msg,source: 'stock_name'});
-        }else{
-        // Eng stock price + other market
-          var msg = 'Stock Symbol: ' + body[0].t + ' Market:' + body[0].e + ' Price ' + body[0].l + ' Change ' + body[0].c + ' ('+ body[0].cp+'%) As of ' + body[0].lt;
-          return res.json({speech: msg,displayText: msg,source: 'stock_name'});
-        } */ 
+        /*get fb User Information from Facebook, read: https://discuss.api.ai/t/how-to-get-user-information-from-facebook/465 */
+        /*if apiai send user info = symbols + price*/
 
+        if(last_price) {
+          msg = 'ชื่อหุ้น ' + result[0].t + ' ราคา ณ ปัจจุบัน อยู่ที่ ' + result[0].l + ' บาท ข้อมูล ณ ' + result[0].lt;
+        }else if(closing_price){ 
+          msg = 'ชื่อหุ้น ' + result[0].t + ' ราคาปิดวันก่อนหน้า อยู่ที่ ' + result[0].pcls_fix + ' บาท ข้อมูล ณ ' + result[0].lt;
+        }else {
+          msg = 'blank';
+        } 
+        return res.json({speech: msg,displayText: msg,source: 'stock_name'});
+        //return res.json({"text":"ท่านอยากทราบ","quick_reply": {"content_type":"text","title":["ราคาปัจจุบัน","ราคาเปิด","ราคาปิดล่าสุด"],"payload":"LLo"}});
+        /*end if symbols + pricesymbols + price */
       } else {
-        var errorMessage = 'I cannot find you stock symbol, please try again.';
+        var errorMessage = 'I cannot find your stock symbol, please try again.';
         return res.status(400).json({ status: {code: 400,errorType: errorMessage}});
       }
     });
     /*end AskStock*/ 
-  }else if (!msg && req.body.result.action == 'AskDW') {
-      console.log('*** DW Symbols ***');
-      dwname = req.body.result.parameters['dwname'];   
-      var cun = 0; var msg = ''; var myJSONObject = []; var msgDW = '';
-      var dwUrl = 'http://49.231.7.202:8080/axis2/services/DWService/getDWCalculatorByFormat?secSym='+dwname+'&format=json';
-        
-        request({url: dwUrl,json: true }, function (error, response, body) {
-          if (!error && response.statusCode == 200 && body[0]) {
-            xml2js.parseString(body, function (err, result) {
-              myJSONObject.push(result);
-              var json = JSON.parse(myJSONObject[0]['ns:getDWCalculatorByFormatResponse']['ns:return']);
-                
-              var nn = json.totalRecord;
-                for (cun = 0;cun<nn;cun++){
-                  //if(json['resultSet'][cun].IssuerSym == 'BLS'){
-                    msgDW += 'Underlying ' + json['resultSet'][cun].UnderlyingSym + ' DW: '+ json['resultSet'][cun].SecSym + ' ราคา ' + json['resultSet'][cun].LstPrice + ' ';
-                  //}
-                }
-              console.log(msgDW);
-                return res.json({speech: msgDW,displayText: msgDW,source: 'stock_name'});
-            });
-          }
-
-        })
-    /*end AskDW*/
-  }else {
-    var msg = 'undefined action';
-    return res.json({speech: msg,displayText: msg});
   }
 
 });
